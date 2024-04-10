36e090dd37404d761d77165d04dbcdcd228362a1
Stay-Hack <staysha.l.hackmann@gmail.com>
2024-04-10 11:15:00 -0700

    Complete

---
+++ Seasonal-website-design/src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css'
+import winterImage from'../../Images/winter.jpg'
+import springImage from'../../Images/spring.jpg'
+import summerImage from'../../Images/summer.jpg'
+import fallImage from'../../Images/fall.jpg'
+
+function App() {
+
+  return (
+    <>
+    <header>
+    <h1>Welcome to my seasonal website!</h1>
+    </header>
+    <div className="photoDiv">
+    <div>
+      <img 
+        src={winterImage}
+        alt="Winter"
+      ></img>
+      <p>Wintertime</p>
+    </div>
+    <div>
+    <img 
+        src={springImage}
+        alt="Spring"
+      ></img>
+      <p>Springtime</p>
+    </div>
+    <div>
+    <img 
+        src={summerImage}
+        alt="Summer"
+      ></img>
+      <p>Summertime</p>
+    </div>
+    <div>
+    <img 
+        src={fallImage}
+        alt="Fall"
+      ></img>
+      <p>Fall time</p>
+    </div>
+    </div>
+    <div className="textContainer">
+    <h2>Some of my favorite seasonal activities are:</h2>
+      <ul>
+        <li>Building snowpeople in the winter.</li>
+        <li>Hiking in the spring wildflowers.</li>
+        <li>Surfing in the summer sun.</li>
+        <li>Playing in the fall leaves.</li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default App
